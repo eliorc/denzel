18a5b259ff524265d0a2df35ceb6e9aeece27594
Elior Cohen <elior.cohen.p@gmail.com>
2018-04-28 21:57:40 +0300

    Cancelled monitoring. Class based task for model in memory holding

--- denzel/app/logic.py
+++ denzel/app/logic.py
@@ -1,3 +1,4 @@
+# -------- Handled by falcon --------
 def verify_input(json_data):
     # TODO - DSG - verify the input data format
     if 'callback_uri' not in json_data:
@@ -6,11 +7,18 @@ def verify_input(json_data):
     return json_data
 
 
+# -------- Handled by celery --------
+
 def process(json_data):
     # TODO - DSG - transform data into model ready data. Must return list, where each element is an example.
-    return []
+    return {}
 
 
-def predict(data):
+def load_model():
+    # TODO - DSG - load model from disk to memory
+    return  # return the loaded model object
+
+
+def predict(model, data):
     # TODO - DSG - Perform prediction. Must return dictionary of {<id>: <result>}
     return {'id': 42, 'id2': 42}
--- denzel/app/tasks.py
+++ denzel/app/tasks.py
@@ -2,7 +2,7 @@ import os
 
 import celery
 import requests
-from app.logic import process, predict
+from app.logic import process, load_model, predict
 
 CELERY_BROKER = os.environ.get('CELERY_BROKER')
 CELERY_BACKEND = os.environ.get('CELERY_BACKEND')
@@ -10,16 +10,29 @@ CELERY_BACKEND = os.environ.get('CELERY_BACKEND')
 app = celery.Celery('tasks', broker=CELERY_BROKER, backend=CELERY_BACKEND)
 
 
-@app.task
+class Model(celery.Task):
+
+    def __init__(self):
+        self._model = load_model()
+
+    @property
+    def model(self):
+        return self._model
+
+
+@app.task(base=Model)
 def invoke_predict(json_data):
     # Preprocess data
     data = process(json_data)
 
     # Preform predictions
-    result = [predict(sample) for sample in data]
+    result = [predict(invoke_predict.model, sample) for sample in data]
 
     # Send prediction to callback_uri
     requests.post(url=json_data['callback_uri'],
                   json=result)
 
     return result
+
+
+Model = app.register_task(Model())
